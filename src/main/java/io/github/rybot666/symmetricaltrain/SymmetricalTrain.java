28a3e5564beb905729e08fc6c93afe4796b5eb62
Rybot666 <ryland.hugo@gmail.com>
2022-02-06 19:58:29 +0000

    Initial Commit

---
+++ src/main/java/io/github/rybot666/symmetricaltrain/SymmetricalTrain.java
@@ -0,0 +1,17 @@
+package io.github.rybot666.symmetricaltrain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SymmetricalTrain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
