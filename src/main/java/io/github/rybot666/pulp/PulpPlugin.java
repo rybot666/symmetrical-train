7f8328a6c4b5fbd2320412f54e4e1730ad17374b
Rybot666 <ryland.hugo@gmail.com>
2022-02-08 22:03:28 +0000

    Create instrumentation

--- src/main/java/io/github/rybot666/pulp/PulpPlugin.java
+++ src/main/java/io/github/rybot666/pulp/PulpPlugin.java
@@ -1,10 +1,14 @@
 package io.github.rybot666.pulp;
 
 import io.github.rybot666.pulp.mixinservice.PulpMixinService;
+import net.bytebuddy.agent.ByteBuddyAgent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.instrument.Instrumentation;
 
 public final class PulpPlugin extends JavaPlugin {
     public static final String NAME = "Pulp";
+    public static final Instrumentation INSTRUMENTATION = ByteBuddyAgent.install();
 
     private static boolean hasInitialised = false;
 
